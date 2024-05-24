521924e20df23325a2583e10add5233e6a6d6d4d
Rizky Ramadhan648 <72913666+rizkyramaddhan@users.noreply.github.com>
2024-05-24 16:24:02 +0700

    Add files via upload
    PortoFolio Version 1.0

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,4 +18,21 @@ $(document).ready(function () {
     backDelay: 500, // menambahkan delay sebelum mulai mengetik string baru
     loop: true,
   });
+
+  // owlCarousel2
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 3,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      900: {
+        items: 2,
+      },
+    },
+  });
 });
